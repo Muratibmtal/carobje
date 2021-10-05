692a695a454801244739eb4713d9656b9e216a8e
Murat <muratibmtal@gmail.com>
2021-10-05 16:09:46 +0300

    Initial commit

---
+++ src/carobje/car.java
@@ -0,0 +1,27 @@
+package carobje;
+
+public class car {
+
+	// deðiþkenler
+	private String caption;
+	private int price;
+	private String brand;
+
+	// set methodlarý
+	public void setCaption(String caption) {
+		this.caption = caption;
+	}
+
+	public void setPrice(int price) {
+		this.caption = caption;
+	}
+
+	public void setBrand(  {
+			   this.price=price;
+		   }this.brand=brand;}
+
+	// get methodlarý
+	public String getCaption() {
+    	  
+    	 return this.caption;{
+      }
